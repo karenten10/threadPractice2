a194f9ce4dc909dc5645acd4f65cc0c7034f4de9
KarenWu <karenten10@gmail.com>
2020-10-03 15:24:53 +0800

    first practice

---
+++ src/thread/ThreadPractice2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 ===> " + DateTime.Now);
+            thread1();
+        }
+
+        static void thread1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("2 ===> " + DateTime.Now);
+        }
+    }
+}
