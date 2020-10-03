90d2ab9dc2f7a070480191c86bcb6f4fc6c96482
KarenWu <karenten10@gmail.com>
2020-10-03 21:02:41 +0800

    most close example of vulcan

--- src/AsyncHello2/AsyncHello2/Program.cs
+++ src/AsyncHello2/AsyncHello2/Program.cs
@@ -1,95 +1,35 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
-namespace AsyncHello2
-{
+namespace AsyncHello2 { 
     class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Method1();
+        Method2();
+        Console.ReadKey();
+    }
+
+    public static async Task Method1()
+    {
+        await Task.Run(() =>
         {
-            Console.WriteLine("Hello World!");
-
-            oneByOne();
-            // RunAsync();
-        }
-
-        static async Task RunAsync()
-        {
-            var int1 = Function1();
-
-            var int2 = Function2();
-
-            var int3 = Function3();
-
-            Console.WriteLine($"===> int1 => {int1}");
-            Console.WriteLine($"===> int2 => {int2}");
-            Console.WriteLine($"===> int3 => {int3}");
-        }
-
-        static Task<int> Function1()
-        {
-            Console.WriteLine($"Function1 => a " + DateTime.Now);
-            Thread.Sleep(5000);
-            return Task.Run(() =>
+            for (int i = 0; i < 100; i++)
             {
-                Console.WriteLine($"Function1 => b " + DateTime.Now);
+                Console.Write(" M1");
+            }
+        });
+    }
 
-                return 1;
-            });
-        }
 
-        static Task<int> Function2()
+    public static void Method2()
+    {
+        for (int i = 0; i < 25; i++)
         {
-            Console.WriteLine($"Function2 => a " + DateTime.Now);
-            Thread.Sleep(5000);
-            return Task.Run(() =>
-            {
-                Console.WriteLine($"Function2 => b " + DateTime.Now);
-
-                return 1;
-            });
-        }
-
-        static Task<int> Function3()
-        {
-            Console.WriteLine($"Function3 => a " + DateTime.Now);
-            Thread.Sleep(5000);
-            return Task.Run(() =>
-            {
-                Console.WriteLine($"Function3 => b " + DateTime.Now);
-
-                return 1;
-            });
-        }
-
-        static void oneByOne()
-        {
-            method1();
-            method2();
-            method3();
-        }
-
-        static void method1()
-        {
-            Task.Delay(3000);
-            //Thread.Sleep(3000);
-            Console.WriteLine($"method1 ===> " + DateTime.Now);
-        }
-
-        static void method2()
-        {
-            Task.Delay(3000);
-            //Thread.Sleep(3000);
-            Console.WriteLine($"method2 ===> " + DateTime.Now);
-        }
-
-        static void method3()
-        {
-            Task.Delay(3000);
-            //Thread.Sleep(3000);
-            Console.WriteLine($"method3 ===> " + DateTime.Now);
-
+            Console.Write(" M2");
         }
     }
+}
+
 }
