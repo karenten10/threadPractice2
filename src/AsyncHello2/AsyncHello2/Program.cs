1891a91f5bca8719f79e6dd36ea2e9d852e69eed
KarenWu <karenten10@gmail.com>
2020-10-03 17:51:39 +0800

    async hello2 for the first time

---
+++ src/AsyncHello2/AsyncHello2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncHello2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            oneByOne();
+        }
+
+        static void oneByOne()
+        {
+            method1();
+            method2();
+        }
+
+        static void method1()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine($"method1 ===> " + DateTime.Now);
+        }
+
+        static void method2()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine($"method2 ===> " + DateTime.Now);
+        }
+    }
+}
